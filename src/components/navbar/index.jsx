d6c4aa6e8cbabaae07b42a67c868ed1fba6d6120
Vila Juan Cruz <yunagosh7@gmail.com>
2023-01-01 19:51:32 -0300

    Rehice todo haciendolo mobile first e hice la navbar y termina la homepage

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,14 +1,51 @@
-import React from 'react'
-import Logo from '../../assets/shared/logo.svg'
+import React from "react";
+import { useState } from "react";
+import Logo from "../../assets/shared/logo.svg";
+import openMenu from "../../assets/shared/icon-hamburger.svg";
+import closeMenu from "../../assets/shared/icon-close.svg";
+import "./navbar.css";
 
 export const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <header className='w-screen fixed flex'>
-        <img src={Logo} alt=""/>
-        <div className="line"></div>
-        <nav className="flex">
-        <a href="/">00 HOME</a><a href="/destinatation">01 DESTINATATION</a><a href="/crew">02 CREW</a><a href="/technology">03 TECHNOLOGY</a>
-        </nav>
+    <header className="w-screen z-50 fixed flex items-center justify-between px-8 pt-12 md:pt-0 md:pl-8 md:pr-0 lg:mt-12 ">
+      <img src={Logo} className="h-12 w-12 " alt="Logo" />
+
+      <nav
+        className={`flex absolute flex-col px-20 nav h-screen w-[70vw] md:flex-row md:static md:w-auto md:h-full md:pt-0 md:px-8 lg:px-16  ${
+          toggle ? "active" : ""
+        }  `}
+      >
+        <img
+          src={closeMenu}
+          alt="Button close menu"
+          className="cursor-pointer md:hidden h-6 w-6 self-end"
+          onClick={() => {
+            setToggle(false);
+          }}
+        />
+        <a className="mx-4 py-10" href="/">
+          00 HOME
+        </a>
+        <a className="mx-4 py-10" href="/destinatation">
+          01 DESTINATATION
+        </a>
+        <a className="mx-4 py-10" href="/crew">
+          02 CREW
+        </a>
+        <a className="mx-4 py-10" href="/technology">
+          03 TECHNOLOGY
+        </a>
+      </nav>
+      <img
+        src={openMenu}
+        alt="Button open menu"
+        className="cursor-pointer md:hidden h-6"
+        onClick={() => {
+          setToggle(true);
+        }}
+      />
     </header>
-  )
-}
+  );
+};
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,20 +3,22 @@ import "./home.css";
 
 export const Home = () => {
   return (
-    <section className="home flex justify-between w-screen place-items-center h-screen bg-no-repeat bg-cover px-20">
-      <article className="w-5/12">
-        <h5>SO, YOU WANT TO TRAVEL TO</h5>
-        <h1>SPACE</h1>
-        <p className="text-lilac p-4">
+    <section className="home">
+    <div className="home-content lg:flex lg:flex-row lg:p-20">
+      <article className="mt-20 flex flex-col items-center lg:w-3/6  ">
+        <h5 className="text-lilac">SO, YOU WANT TO TRAVEL TO</h5>
+        <h1 className="">SPACE</h1>
+        <p className="text-lilac px-4 md:w-3/6 lg:w-full ">
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
           back, and relax because we’ll give you a truly out of this world
           experience!
         </p>
       </article>
-      <button className="btn-explore">
+      <button className="btn-explore ">
         EXPLORE
       </button>
+    </div>
     </section>
   );
 };
